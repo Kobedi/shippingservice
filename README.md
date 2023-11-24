4074bfc51d1836d588f99791631e211e0cde5577
makgadk2@gmail.com <makgadk2@gmail.com>
2023-11-24 05:20:35 +0200

    Initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+## New Order fulfilment Service
+
+This is a project struture for the shipping service that will house integrates with the other speedorder
+microservice to make shipping the customer order to the destination address.  
+
+ 
+### Relevant articles:
+    
---
+++ src/test/java/com/zadscience/shipping/service/shippingservice/ShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zadscience.shipping.service.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
